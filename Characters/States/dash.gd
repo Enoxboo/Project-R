86d3b28ef656130b0b848af5ca5e5c1527d05b8e
Enoxboo <matteo.marquant@ynov.com>
2025-10-31 14:22:07 +0100

    feat(dash): add dash burst particles

--- Characters/States/dash.gd
+++ Characters/States/dash.gd
@@ -2,18 +2,26 @@ extends CharacterState
 
 func enter() -> void:
 	character.can_dash = false
-	var input_dir = character.get_input_direction()
-	var dash_direction: Vector2
-	if input_dir:
-		dash_direction = input_dir
-	else: 
-		dash_direction = character.last_direction
+	
+	var input_dir := character.get_input_direction()
+	var dash_dir := input_dir if input_dir else character.last_direction
+	
+	_set_dash_particles_direction(-dash_dir)
+	
+	character.velocity = dash_dir * character.data.dash_force
 	character.dash_duration_timer.start()
 	character.dash_cooldown_timer.start()
-	character.velocity = dash_direction * character.data.dash_force
+	
+	character.start_dash_particles()
+
 
 func physics_update(_delta: float) -> void:
 	character.move_and_slide()
+
+
+func _set_dash_particles_direction(direction: Vector2) -> void:
+	var material := character.dash_burst.process_material
+	material.direction = Vector3(direction.x, direction.y, 0.0)
 
 
 func _on_dash_duration_timer_timeout() -> void:
--- Characters/character.gd
+++ Characters/character.gd
@@ -4,6 +4,7 @@ class_name Character
 
 @onready var dash_duration_timer: Timer = $DashDurationTimer
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
+@onready var dash_burst: GPUParticles2D = $DashBurst
 
 @export var data: CharacterData
 
@@ -11,6 +12,7 @@ var can_dash = true
 var last_direction: Vector2
 
 func _ready() -> void:
+	dash_burst.process_material = dash_burst.process_material.duplicate()
 	dash_duration_timer.wait_time = data.dash_time
 	dash_cooldown_timer.wait_time = data.dash_cooldown
 
@@ -19,3 +21,6 @@ func get_input_direction() -> Vector2:
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down")
 	).normalized()
+
+func start_dash_particles() -> void:
+	dash_burst.emitting = true
