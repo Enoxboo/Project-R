120d063601a0749aa32f746bdbcdb4373ab97a21
Enoxboo <matteo.marquant@ynov.com>
2025-12-08 23:04:35 +0100

    feat(demo): improve first time playing demo experience

--- Levels/Demo/room_3.gd
+++ Levels/Demo/room_3.gd
@@ -13,14 +13,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		label.text = "bravo"
 		portal.visible = true
 		portal.monitoring = true
-	else:
-		label.text = "bouffon"
-
-func _on_area_2d_area_exited(_area: Area2D) -> void:
-	if portal.visible:
-		return
-	else:
-		label.text = "Passe par dessus ce truc pour te give un compagnon
-Une fois être passé dessus une fois, tu peux switch sur un deuxième personnage
-Ce personnage a des stats modifiées et une toute nouvelle attaque
-"
+		area_2d.visible = false
+		area_2d.monitoring = false
